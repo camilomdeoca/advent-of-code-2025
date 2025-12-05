fb9c48e80872e55c9c14ca82f8c5f6a5891df43d
Camilo Montes de Oca <camilomdeoca@gmail.com>
2025-12-05 04:41:26 -0300

    day 5 part 2

--- day5/main.cpp
+++ day5/main.cpp
@@ -1,6 +1,8 @@
+#include <algorithm>
 #include <cassert>
 #include <cstdint>
 #include <fstream>
+#include <iterator>
 #include <print>
 #include <string>
 #include <vector>
@@ -19,7 +21,7 @@ int main(int argc, char *argv[])
 
     struct Range { uint64_t start; uint64_t end; };
 
-    std::vector<Range> ranges;
+    std::vector<Range> ranges, ranges_filter_aux;
 
     while (std::getline(ifs, line))
     {
@@ -33,27 +35,54 @@ int main(int argc, char *argv[])
         ranges.emplace_back(id_start, id_end);
     }
 
-    uint64_t fresh_count = 0;
-
-    while (std::getline(ifs, line))
+    bool id_ranges_changed = true;
+    size_t pass = 0;
+    while (id_ranges_changed)
     {
-        uint64_t ingredient_id;
-        std::stringstream ss(line);
-        ss >> ingredient_id;
-        // std::println("{}", ingredient_id);
-        bool is_fresh = false;
-        for (const Range &range : ranges)
+        id_ranges_changed = false;
+        // std::println("PASS {}", ++pass);
+        std::vector<size_t> to_be_deleted_idx;
+        for (size_t j = 0; j < ranges.size(); j++)
         {
-            if (ingredient_id >= range.start && ingredient_id <= range.end)
+            for (size_t i = 0; i < ranges.size(); i++)
             {
-                is_fresh = true;
-                fresh_count++;
-                break;
+                if (i == j) continue;
+
+                if (ranges[i].start >= ranges[j].start && ranges[i].start <= ranges[j].end)
+                {
+                    ranges[i].start = ranges[j].end + 1;
+                    id_ranges_changed = true;
+                }
+                
+                if (ranges[i].end >= ranges[j].start && ranges[i].end <= ranges[j].end)
+                {
+                    // Should check for underflow but there isnt any low numbers
+                    // in ranges in input.txt so it doesnt matter
+                    ranges[i].end = ranges[j].start - 1;
+                    id_ranges_changed = true;
+                }
             }
         }
+
+        std::copy_if(
+            ranges.begin(),
+            ranges.end(),
+            std::back_inserter(ranges_filter_aux),
+            [](const Range &range) { return range.end >= range.start; }
+        );
+
+        std::swap(ranges_filter_aux, ranges);
+        ranges_filter_aux.clear();
     }
 
-    std::println("Fresh count: {}", fresh_count);
+    uint64_t fresh_ids_count = 0;
+    for (const Range &range : ranges)
+    {
+        std::println("{}-{}", range.start, range.end);
+        fresh_ids_count += range.end - range.start + 1;
+    }
+
+    std::println("Fresh IDs count: {}", fresh_ids_count);
 
     return 0;
 }
