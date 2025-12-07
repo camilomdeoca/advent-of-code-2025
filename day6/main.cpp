0d50b24ab5aea40166ac012ff5cd21f90505d3af
Camilo Montes de Oca <camilomdeoca@gmail.com>
2025-12-06 23:59:36 -0300

    day 6 part 2

--- day6/main.cpp
+++ day6/main.cpp
@@ -15,79 +15,64 @@ int main(int argc, char *argv[])
 
     std::ifstream ifs(argv[1]);
 
+    std::vector<std::string> lines;
     std::string line;
-
-    constexpr size_t NUMS_PER_COL = 4;
-
-    std::vector<std::array<uint64_t, NUMS_PER_COL>> numbers;
-    uint64_t x_size = 0;
-
-    for (size_t y = 0; y < NUMS_PER_COL; y++)
+    while (std::getline(ifs, line))
     {
-        std::getline(ifs, line);
-
-        char *cursor = line.data();
-        const char *end = &line.back();
-        int x = 0;
-        while (cursor < end)
-        {
-            if (y == 0) numbers.emplace_back();
-            uint64_t num = std::strtoul(cursor, &cursor, 10);
-            // std::print("{} ", num);
-            numbers[x][y] = num;
-            x++;
-        }
-        // std::println();
+        lines.emplace_back();
+        std::swap(lines.back(), line);
     }
+    std::println();
 
-    // std::println();
-    // for (const auto &col : numbers)
-    // {
-    //     for (const auto num : col)
-    //     {
-    //         std::print("{} ", num);
-    //     }
-    //     std::println();
-    // }
-    // std::println();
+    const size_t line_length = lines.front().length();
+    
+    // all lines should be the same length
+    for (const auto &line : lines) assert(line.length() == line_length);
 
     uint64_t sum = 0;
 
+    std::vector<uint64_t> numbers;
+    size_t char_idx = line_length - 1;
+    for (int64_t char_idx = line_length - 1; char_idx >= 0; char_idx--)
     {
-        std::getline(ifs, line);
-        // std::println("{}", line);
-        size_t col_idx = 0;
-        auto it = line.begin();
-        while (it != line.end())
+        std::string number_string;
+        for (size_t i = 0; i < lines.size() - 1; i++)
         {
-            while (it != line.end() && *it == ' ') it++;
-            if (it == line.end()) break;
-
-            uint64_t acc;
-            switch (*it) {
-            case '+':
-                acc = 0;
-                for (const auto num : numbers[col_idx]) acc += num;
-                break;
-            case '*':
-                acc = 1;
-                for (const auto num : numbers[col_idx]) acc *= num;
-                break;
-            default:
-                std::println("??????");
-                continue;
-                break;
-            }
-            // std::print("{} ", acc);
-            it++;
-            sum += acc;
-            col_idx++;
+            char c = lines[i][char_idx];
+            if (c != ' ') number_string.push_back(c);
         }
-        // std::println();
+
+        // If its a blank column we should skip
+        if (number_string.empty()) continue;
+
+        // std::println("`{}`", number_string);
+        uint64_t number = std::stoul(number_string);
+        numbers.push_back(number);
+
+        // Thankfully all the operations are always in the last column of the problem
+        // so when there is an operator in the last row we can operate on all the numbers
+        // collected and then free the `numbers` vector.
+        char operation = lines.back()[char_idx];
+        uint64_t acc;
+        switch (operation) {
+        case ' ':
+            break;
+        case '+':
+            acc = 0;
+            for (const uint64_t num : numbers) acc += num;
+            numbers.clear();
+            sum += acc;
+            break;
+        case '*':
+            acc = 1;
+            for (const uint64_t num : numbers) acc *= num;
+            numbers.clear();
+            sum += acc;
+            break;
+        }
     }
 
-    std::println("sum {}", sum);
-
+    std::println("Sum: {}", sum);
 
     return 0;
 }
